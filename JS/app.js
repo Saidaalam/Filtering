d2b9a0db74df7c7f614b600df1da935b95c66b36
Saidaalam <133933105+Saidaalam@users.noreply.github.com>
2023-09-13 22:37:08 +0600

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,13 @@
+let mixer = mixitup(".wrapper");
+
+$(document).ready(function () {
+
+    $(".slider").slick({
+      slidesToShow: 3,
+      arrows: true,
+      prevArrow: ".leftArrows",
+      nextArrow: ".rightArrows",
+      centerPadding: "0px",
+    });
+  
+  });
